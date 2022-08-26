092f9f8edaefc3a58a9b45efae176124ed7e2335
DaisyOdongo <daisy.odongo@student.moringaschool.com>
2022-08-26 10:40:45 +0300

    commit tree 1d65d337af46a13908ff01d355b012fa98feb1ea

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
 }
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
 
 /* 
   Add your pseudocode here
@@ -15,6 +22,30 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+  console.log("");
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
